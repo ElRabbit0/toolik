14d791148a8c0982a334b918279043bd0c197756
ElRabbit0 <vovavova2007@gmail.com>
2025-09-06 23:38:46 +1000

    test

--- friends/main.php
+++ friends/main.php
@@ -6,4 +6,5 @@ $mysql = new mysqli($hostnameSQL, $usernameSQL, $passwordSQL, $databaseSQL);
 echo "hi";
 $mysql->close();
 // friend, bestfriend, partner, subscriber, following
+// систему приглашений и сверстай страницу
 ?>
