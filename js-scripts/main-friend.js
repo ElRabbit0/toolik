08ae30210d351cfd340850b37531f7ed5a5a147d
ElRabbit0 <vovavova2007@gmail.com>
2025-09-12 23:09:10 +1000

    friend HTML ready

---
+++ js-scripts/main-friend.js
@@ -0,0 +1,117 @@
+let friendBut = document.getElementById('friend-but');
+let requestBut = document.getElementById('request-but');
+let friendList = document.getElementById('friend-list-container');
+let requestList = document.getElementById('request-list-container');
+let Pos = "";
+
+let exitButtons = document.querySelectorAll('.exit');
+let openButtons = document.querySelectorAll('.h-open');
+let search_form = document.getElementById('search-form');
+let subscriber_list = document.getElementById('subscriber-list');
+let following_list = document.getElementById('following-list');
+
+startPage();
+friendBut.addEventListener('click',()=>{
+    swapForm("friend");
+});
+requestBut.addEventListener('click',()=>{
+    swapForm("request");
+});
+
+exitButtons.forEach((Elem)=>{
+    Elem.addEventListener('click',()=>{
+        hideForm(Elem.id);
+    });
+});
+openButtons.forEach((Elem)=>{
+    Elem.addEventListener('click',()=>{
+        hideForm(Elem.id);
+    });
+});
+
+function startPage(){
+    friendList.style.display = "block";
+    requestList.style.display = "none";
+    friendBut.classList = 'page-header-button-active';
+    Pos = "friend";
+    hideForm();
+}
+function swapForm(form){
+    switch(form){
+        case "friend":
+            if(Pos !== "friend"){
+                friendList.style.display = "block";
+                requestList.style.display = "none";
+                friendBut.classList = 'page-header-button-active';
+                requestBut.classList = 'page-header-button'; 
+                Pos = "friend";
+            }
+            break;
+        case "request":
+            if(Pos !== "request"){
+                friendList.style.display = "none";
+                requestList.style.display = "block";
+                friendBut.classList = 'page-header-button';
+                requestBut.classList = 'page-header-button-active';
+                Pos = "request";
+            }
+            break;
+    }
+}
+function hideForm(form){
+    switch(form){
+        case "search_form_exit":
+            if(search_form.style.display != 'none'){
+                search_form.style.display = 'none';
+            }
+            else{
+                search_form.style.display = 'block';
+            }
+            break;
+        case "subscriber_list_exit":
+            if(subscriber_list.style.display != 'none'){
+                subscriber_list.style.display = 'none';
+            }
+            else{
+                subscriber_list.style.display = 'block';
+            }
+            break;
+        case "following_list_exit":
+            if(following_list.style.display != 'none'){
+                following_list.style.display = 'none';
+            }
+            else{
+                following_list.style.display = 'block';
+            }
+            break;
+        case "search-form-open":
+            if(search_form.style.display != 'none'){
+                search_form.style.display = 'none';
+            }
+            else{
+                search_form.style.display = 'block';
+            }
+            break;
+        case "subscriber-list-open":
+            if(subscriber_list.style.display != 'none'){
+                subscriber_list.style.display = 'none';
+            }
+            else{
+                subscriber_list.style.display = 'block';
+            }
+            break;
+        case "following-list-open":
+            if(following_list.style.display != 'none'){
+                following_list.style.display = 'none';
+            }
+            else{
+                following_list.style.display = 'block';
+            }
+            break;
+        default:
+            search_form.style.display = 'none';
+            subscriber_list.style.display = 'none';
+            following_list.style.display = 'none';
+            break;
+    }
+}
---
+++ node/friend-in-list.php
@@ -0,0 +1,38 @@
+<div class="friend-node">
+    <div class="account-icon">
+        <?php
+        if ($friendIcon != '' && $friendIcon != ' ') {
+            echo '<img class="user-icon" src="' . $targetDirIcon . $friendIcon . '" alt="">';
+        } else {
+            echo '<img class="not-icon" src="../img/icon.png" alt="">';
+        }
+        ?>
+    </div>
+    <div class="account-nickname">
+        <div class="nickname-in">
+            <?php
+            echo $friendNickname;
+            ?>
+        </div>
+        <div class="status-in">
+            <span>Статус: </span><?php
+            echo $friendStat;
+            ?>
+        </div>
+        <div class="date-in">
+            <span>Начало дружбы: </span><?php
+            echo $friendDate;
+            ?>
+        </div>
+    </div>
+    <div class="buttons-friend">
+        <form action="/pages/logout.php" method="post" class="add-form">
+            <button class="func-button black-button" type="submit" name="status"
+                value="<?php $friendId ?>">Статус</button> <!-- Поменять -->
+        </form>
+        <form action="/pages/logout.php" method="post" class="del-form">
+            <button class="func-button del-friend-button" type="submit" name="run_script "
+                value="<?php $friendId ?>">Удалить</button>
+        </form>
+    </div>
+</div>
